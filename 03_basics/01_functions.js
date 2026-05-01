8360fb8e75723ed85f758ff632a6e4a1da704b84
SohaibKhattak <113389128+SohaibKhattak@users.noreply.github.com>
2026-05-01 11:44:43 +0000

    obj and arrays  passing in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,12 +27,52 @@ function userLoggedInMessage(username){
     if(!username){ 
         //(username === undefined) is = (!username)
     //in JS undefined & "" are treated as false
-        console.log("please enter your name")
-        return 
+        // console.log("please enter your name")
+        // return 
     }
-    return `${username} just logged in`
+    // return `${username} just logged in`
 }
 
-console.log(userLoggedInMessage()) 
+// console.log(userLoggedInMessage()) 
 
 // if we dont pass any value...print will be undefined just logged in...bcz we haven't passed on any value.
+
+//------------------Rest Operator--------------------
+//Suppose we are making an ecommerce website, in their we have a cart which is used to add products to add up at end. User added 1 element to the cart. Now till here it is fine...but what if he adds on more products to the cart?
+//Here we use Rest operator.: One variable is passed as a parameter and we can add multiple values as argument when we call the function.
+
+function calculateCartPrice(...num){
+    return num
+}
+
+// console.log(calculateCartPrice(200,100))
+
+//Object passing to function
+
+const user = {
+    username : "Sohaib",
+    price : 199
+}
+
+function handleObject(anyObject){
+    // console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+// we can also create object directly when calling the function
+
+handleObject({
+    username: "Sohaib Akhtar",
+    price : 399
+})
+
+//---------------------array in functions-------
+
+const arr = [1,2,4]
+
+function handleArray(anyArray){
+    return anyArray[1]
+}
+
+console.log(handleArray(arr))
+console.log(handleArray([200,100,400]))
