341b86eb0b0b225be1035a11c8bc97642490f5de
SohaibKhattak <113389128+SohaibKhattak@users.noreply.github.com>
2026-05-02 09:49:45 +0000

    while and do while loops

---
+++ 05_iterations/while_doWhile_loops.js
@@ -0,0 +1,33 @@
+//while loop
+
+// let index = 0;
+// while(index <=10){
+//     console.log(index)
+//     index = index +2
+// }
+
+let myArr = ["Sohaib", "Ali", "Arshad"]
+
+// let arr= 0
+// while(arr< myArr.length){
+//     console.log(myArr[arr])
+//     arr++
+// }
+
+// do while----------------------
+
+// let score = 1
+
+// do{
+//     console.log(`Score is ${score}`)
+//     score = score +1
+// }while(score <= 10)
+
+
+// when condition is false at start, do while still runs 1 time
+let score = 1
+
+do{
+    console.log(`Score is ${score}`)
+    score = score +1
+}while(score < 1)
